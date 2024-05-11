9a2db58f8a7bfbc7fdc6b866ef0da8d3992c4c30
milaadelia16 <168887392+milaadelia16@users.noreply.github.com>
2024-05-11 10:29:06 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 
-include "app/Mahasiswa.php";
-include "inc/Mahasiswa.php";
-include "layout/Mahasiswa.php";
+require "Mobil/Mobil.php";
+require "Mobil/Toyota.php";
+require "Mobil/Honda.php";
+require "Calculator.php";
 
-$app = new App\Mahasiswa();
-echo "<br>";
-$inc = new inc\Mahasiswa();
-echo "<br>";
-$layout = new layout\Mahasiswa();
+require "vendor/autoload.php";
+
+$Toyota = new Dummy\Mobil\Toyota(" Toyota Fortuner ", 10);
+$calculator = new Dummy\Calculator($Toyota);
+echo "jarak maksimum ".$Toyota->getMerk().$calculator->hitungJarak()."Km";
+echo "<hr>";
+$Honda = new Dummy\Mobil\Honda("Honda CRV", 10);
+$calculator = new Dummy\Calculator($Honda);
+echo "jarak maksimum ".$Honda->getMerk().$calculator->hitungJarak()."Km";
+
